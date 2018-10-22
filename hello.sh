3b015a7293cac1bbef733a2ff29c8484e210409f
Sulabasana <piotr.frydryszak@gmail.com>
2018-10-22 19:55:48 +0200

    Weak and strong quoting added

--- hello.sh
+++ hello.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
-#echo "Hello World"
+# #!/bin/bash
+# echo "Hello World"
 
-#!/usr/bin/env bash
-# Note that spaces cannot be used around the `=` assignment operator
-whom_variable="World"
-# Use printf to safely output the data
-printf "Hello, %s\n" "$whom_variable"
-#> Hello, World
+# #!/usr/bin/env bash
+# # Note that spaces cannot be used around the `=` assignment operator
+# whom_variable="World"
+# # Use printf to safely output the data
+# printf "Hello, %s\n" "$whom_variable"
+# #> Hello, World
+
+#Weak quoting with ""
+world="World"
+echo "Hello $world"
+
+#Strong quoting with ''
+world="World"
+echo 'Hello $world'
