45e46fa1ee5fd0b1fb5028cb454f9b40e83c45f7
Piotr Frydryszak <piotr.frydryszak@gmail.com>
2018-10-29 21:41:20 +0100

    Listing with shell
    Listing with shell

---
+++ Listing.sh
@@ -0,0 +1,15 @@
+# display the files and directories that are in the current directory
+printf "%s\n" *
+# display only the directories in the current directory
+printf "%s\n" */
+# display only (some) image files
+printf "%s\n" *.{gif,jpg,png}
+
+
+
+# display the files and directories that are in the current directory
+printf "%s\n" *
+# display only the directories in the current directory
+printf "%s\n" */
+# display only (some) image files
+printf "%s\n" *.{gif,jpg,png}
